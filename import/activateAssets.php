b1ab6600f2a782dda9cc14ca79657f5de52f28ea
Lennart Demes <31403260+StruffelProductions@users.noreply.github.com>
2021-10-30 22:04:38 +0200

    Change sorting for activateAssets back to "random"

--- import/activateAssets.php
+++ import/activateAssets.php
@@ -13,7 +13,7 @@
 	$query->include->internal=true;
 	$query->include->creator = true;
 	$query->limit = 2;
-	$query->sort="oldest";
+	$query->sort="random";
 	$assetsToActivate = loadAssetsFromDatabase($query);
 	foreach ($assetsToActivate->assets as $a) {
 		$creatorId = $a->creator->creatorId;
