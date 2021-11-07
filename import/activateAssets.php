add07d5efa6f77bccfd02daa44b53e9f46550c61
Lennart Demes <31403260+StruffelProductions@users.noreply.github.com>
2021-11-07 11:44:49 +0100

    Switch activation order to "latest"

--- import/activateAssets.php
+++ import/activateAssets.php
@@ -13,7 +13,7 @@
 	$query->include->internal=true;
 	$query->include->creator = true;
 	$query->limit = 2;
-	$query->sort="random";
+	$query->sort="latest";
 	$assetsToActivate = loadAssetsFromDatabase($query);
 	foreach ($assetsToActivate->assets as $a) {
 		$creatorId = $a->creator->creatorId;
