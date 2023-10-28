736860367408f503b73423c254a1c64ef8b4326b
Lennart Demes <31403260+struffel@users.noreply.github.com>
2023-10-28 11:03:40 +0200

    Comment out unused fields

--- src/include/data.php
+++ src/include/data.php
@@ -103,14 +103,14 @@ enum QUIRK : int {
 	case SIGNUP_REQUIRED = 1;
 	case PAYMENT_REQUIRED = 2;
 	case ADS = 3;
-	case ASSET_PACK = 4;
+	#case ASSET_PACK = 4;
 
 	public function slug() : string{
 		return match ($this){
 			QUIRK::SIGNUP_REQUIRED => 'sign-up',
 			QUIRK::PAYMENT_REQUIRED => 'payment',
 			QUIRK::ADS => 'ads',
-			QUIRK::ASSET_PACK => 'asset-pack'
+			#QUIRK::ASSET_PACK => 'asset-pack'
 		};
 	}
 
@@ -118,8 +118,8 @@ enum QUIRK : int {
 		return match ($this){
 			QUIRK::SIGNUP_REQUIRED => 'Sign-up required',
 			QUIRK::PAYMENT_REQUIRED => 'Payment required',
-			QUIRK::ADS => 'Advertisements',
-			QUIRK::ASSET_PACK => 'Asset Packs'
+			QUIRK::ADS => 'On-site ads',
+			#QUIRK::ASSET_PACK => 'Asset Packs'
 		};
 	}
 
@@ -176,24 +176,24 @@ enum TYPE : int {
 enum LICENSE : int {
 	case CUSTOM = 0;
 	case CC0 = 1;
-	case CC_BY = 2;
-	case CC_BY_SA = 3;
-	case CC_BY_NC = 4;
+	#case CC_BY = 2;
+	#case CC_BY_SA = 3;
+	#case CC_BY_NC = 4;
 	case CC_BY_ND = 5;
-	case CC_BY_NC_SA = 6;
-	case CC_BY_NC_ND = 7;
+	#case CC_BY_NC_SA = 6;
+	#case CC_BY_NC_ND = 7;
 	case APACHE_2_0 = 8;
 
 	public function slug(): string {
 		return match ($this) {
 			LICENSE::CUSTOM => 'custom',
 			LICENSE::CC0 => 'cc-0',
-			LICENSE::CC_BY => 'cc-by',
-			LICENSE::CC_BY_SA => 'cc-by-sa',
-			LICENSE::CC_BY_NC => 'cc-by-nc',
+			#LICENSE::CC_BY => 'cc-by',
+			#LICENSE::CC_BY_SA => 'cc-by-sa',
+			#LICENSE::CC_BY_NC => 'cc-by-nc',
 			LICENSE::CC_BY_ND => 'cc-by-nd',
-			LICENSE::CC_BY_NC_SA => 'cc-by-nc-sa',
-			LICENSE::CC_BY_NC_ND => 'cc-by-nc-nd',
+			#LICENSE::CC_BY_NC_SA => 'cc-by-nc-sa',
+			#LICENSE::CC_BY_NC_ND => 'cc-by-nc-nd',
 			LICENSE::APACHE_2_0 => 'apache-2-0',
 		};
 	}
@@ -202,12 +202,12 @@ enum LICENSE : int {
 		return match ($this) {
 			LICENSE::CUSTOM => 'Custom License - Check Website',
 			LICENSE::CC0 => 'Creative Commons CC0',
-			LICENSE::CC_BY => 'Creative Commons BY',
-			LICENSE::CC_BY_SA => 'Creative Commons BY-SA',
-			LICENSE::CC_BY_NC => 'Creative Commons BY-NC',
+			#LICENSE::CC_BY => 'Creative Commons BY',
+			#LICENSE::CC_BY_SA => 'Creative Commons BY-SA',
+			#LICENSE::CC_BY_NC => 'Creative Commons BY-NC',
 			LICENSE::CC_BY_ND => 'Creative Commons BY-ND',
-			LICENSE::CC_BY_NC_SA => 'Creative Commons BY-NC-SA',
-			LICENSE::CC_BY_NC_ND => 'Creative Commons BY-NC-ND',
+			#LICENSE::CC_BY_NC_SA => 'Creative Commons BY-NC-SA',
+			#LICENSE::CC_BY_NC_ND => 'Creative Commons BY-NC-ND',
 			LICENSE::APACHE_2_0 => 'Apache License 2.0',
 		};
 	}
