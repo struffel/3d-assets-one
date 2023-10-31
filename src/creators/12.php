576a5a6503a1e73da2a1ca925719855232ddc7ee
Lennart Demes <31403260+struffel@users.noreply.github.com>
2023-10-31 21:33:38 +0100

    fix hdri workshop

--- src/creators/12.php
+++ src/creators/12.php
@@ -4,7 +4,7 @@
 
 	class CreatorFetcher12 extends CreatorFetcher{
 
-		private CREATOR $creator = CREATOR::HDRIWORKSHOP;
+		public CREATOR $creator = CREATOR::HDRIWORKSHOP;
 
 		function findNewAssets(array $existingUrls, array $config):AssetCollection{
 
@@ -14,13 +14,16 @@
 			foreach($apiOutput as $hdriWorkshopAsset){
 				if(!in_array($hdriWorkshopAsset['fullUrl'],$existingUrls)){
 					$tmpAsset = new Asset(
+						id: NULL,
 						name: $hdriWorkshopAsset['name'],
 						url: $hdriWorkshopAsset['fullUrl'],
 						tags: explode(" ",$hdriWorkshopAsset['name']),
 						type: TYPE::HDRI,
 						creator: $this->creator,
 						license: LICENSE::CUSTOM,
-						thumbnailUrl: $hdriWorkshopAsset['fullUrlThumb']
+						thumbnailUrl: $hdriWorkshopAsset['fullUrlThumb'],
+						quirks:[],
+						date:date("Y-m-d")
 					);
 
                     $tmpCollection->assets []= $tmpAsset;
