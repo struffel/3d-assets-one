45dc3631f1913cac1f84153ab4c5ce1caa71db4b
Lennart Demes <31403260+struffel@users.noreply.github.com>
2023-11-04 18:11:51 +0100

    Create config for textures.com

---
+++ src/creators/15.php
@@ -0,0 +1,51 @@
+<?php
+
+	// textures.com
+
+	class CreatorFetcher15 extends CreatorFetcher{
+
+		public CREATOR $creator = CREATOR::TEXTURES_COM;
+
+		function findNewAssets(array $existingUrls, array $config):AssetCollection{
+
+			$tmpCollection = new AssetCollection();
+
+			$page = 1;
+
+			do{
+				$apiData = FetchLogic::fetchRemoteJson($config['apiUrlBase'].$page);
+
+				$assetsFoundThisIteration = sizeof($apiData['data']);
+				foreach ($apiData['data'] as $texComAsset) {
+
+					$url = $config['finalUrlBase'].$texComAsset['filenameWithoutSet']."/".$texComAsset['id'];
+
+					if(!in_array($url,$existingUrls)){
+
+						$tmpCollection->assets []= new Asset(
+							id: NULL,
+							name:$texComAsset['defaultPhotoSet']['titleThumbnail'],
+							url: $url,
+							thumbnailUrl: $config['finalUrlBase'].$texComAsset['picture'],
+							date: $texComAsset['defaultPhotoSet']['createdAtUtc'],
+							tags: array_filter(preg_split('/[^A-Za-z0-9]/',$texComAsset['defaultPhotoSet']['titleThumbnail'])),
+							type: TYPE::from($config['categoryMapping'][$texComAsset['defaultCategoryId']]),
+							license: LICENSE::CUSTOM,
+							creator: $this->creator,
+							quirks: [QUIRK::SIGNUP_REQUIRED],
+							status: ASSET_STATUS::INACTIVE
+						);
+					}
+				}
+
+				$page += 1;
+
+			}while($assetsFoundThisIteration > 0 && $page < 20 /* Failsafe */);
+
+			return $tmpCollection;
+		}
+
+		public function fetchThumbnailImage(string $url):string {
+			return ImageLogic::removeUniformBackground(FetchLogic::fetchRemoteData($url),25,25,0.03);
+		}
+	}
