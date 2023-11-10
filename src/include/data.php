0f2154fff0bf485608f77505ab9aaabccb1a56e4
Lennart Demes <31403260+struffel@users.noreply.github.com>
2023-11-10 10:03:11 +0100

    Add new limited downloads quirks

--- src/include/data.php
+++ src/include/data.php
@@ -114,20 +114,24 @@ enum QUIRK : int {
 	#case PAYMENT_REQUIRED = 2;
 	case ADS = 3;
 	case ASSET_PACK = 4;
+	case LIMITED_FREE_DOWNLOADS = 5;
+	#case LIMITED_FREE_QUALITY = 6;
 
 	public function slug() : string{
 		return match ($this){
 			QUIRK::SIGNUP_REQUIRED => 'sign-up',
 			QUIRK::ADS => 'ads',
-			QUIRK::ASSET_PACK => 'asset-pack'
+			QUIRK::ASSET_PACK => 'asset-pack',
+			QUIRK::LIMITED_FREE_DOWNLOADS => 'limited-free-downloads'
 		};
 	}
 
 	public function name() : string{
 		return match ($this){
-			QUIRK::SIGNUP_REQUIRED => 'Sign-up required',
-			QUIRK::ADS => 'On-site ads',
-			QUIRK::ASSET_PACK => 'Asset Packs'
+			QUIRK::SIGNUP_REQUIRED => 'Sign-up Required',
+			QUIRK::ADS => 'On-site Ads',
+			QUIRK::ASSET_PACK => 'Asset Packs',
+			QUIRK::LIMITED_FREE_DOWNLOADS => 'Limited Number of Free Downloads'
 		};
 	}
 
