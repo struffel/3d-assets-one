421ec002a2a798fe046082e91ebec60fa1c2728f
Lennart Demes <31403260+struffel@users.noreply.github.com>
2023-12-08 20:47:04 +0100

    Add new sorting options and status filtering
    Introduces new sorting options to the asset search function and changes the way filtering for asset status works

--- src/include/assets.php
+++ src/include/assets.php
@@ -5,19 +5,21 @@ enum SORTING: string{
 	case LATEST = "latest";
 	case OLDEST = "oldest";
 	case RANDOM = "random";
+	case MOST_CLICKED = "most-clicked";
+	case LEAST_CLICKED = "least-clicked";
+	case MOST_TAGGED = "most-tagged";
+	case LEAST_TAGGED = "least-tagged";
 
 	/**
 	 * Returns the enum value for the string. 
 	 * Every other/invalid string gets turned into SORTING::LATEST.
 	 */
-	public static function fromString(string $string) : SORTING{
-		return match ($string) {
-			"latest" => SORTING::LATEST,
-			"oldest" => SORTING::OLDEST,
-			"random" => SORTING::RANDOM,
-			"popular" => SORTING::POPULAR,
-			default => SORTING::LATEST
-		};
+	public static function fromAnyString(string $string) : SORTING{
+		if(in_array($string,array_column(SORTING::cases(), 'value'))){
+			return SORTING::from($string);
+		}else{
+			return SORTING::LATEST;
+		}
 	}
 }
 
@@ -97,7 +99,7 @@ class AssetQuery{
 
 	){}
 
-	public function toHttpGet() : string{
+	public function toHttpGet(bool $includeStatus = false) : string{
 
 		$enumToSlugConverter = function($e){return $e->slug();};
 
@@ -113,47 +115,64 @@ class AssetQuery{
 		$output['type'] = array_map($enumToSlugConverter,$this->filterType);
 		$output['avoid'] = array_map($enumToSlugConverter,$this->filterAvoidQuirk);
 
+		if($includeStatus){
+			$output['status'] = $this->filterStatus->value ?? NULL;
+		}
+
 		return http_build_query($output);
 	}
 
 	/**
 	 * Generates a new AssetQuery based on the current HTTP GET parameters in $_GET.
+	 * The asset status can be forced to a specific value using the method paramter 'filterStatus'.
+	 * Setting filterStatus to NULL allows the status to be controlled using a HTTP parameter.
 	 */
-	public static function fromHttpGet(ASSET_STATUS $filterStatus = ASSET_STATUS::ACTIVE) : AssetQuery{
+	public static function fromHttpGet(?ASSET_STATUS $filterStatus = ASSET_STATUS::ACTIVE) : AssetQuery{
+
+		// status filter (only if it's not defined in the method head)
+		if($filterStatus === NULL && isset($_GET['status']) && $_GET['status'] != "" ){
+			$filterStatus = ASSET_STATUS::tryFrom(intval($_GET['status']));
+		}
 
 		// assetId filter
 		$filterAssetId = [];
 		foreach($_GET['id'] ?? [] as $assetId) {
 			$filterAssetId []= intval($assetId);
 		}
+		$filterAssetId = array_filter($filterAssetId);
 
 		// creator filter
 		$filterCreator = [];
 		foreach($_GET['creator'] ?? [] as $creatorSlug){
 			$filterCreator []= CREATOR::fromSlug($creatorSlug);
 		}
+		$filterCreator = array_filter($filterCreator);
 
 		// type filter
 		$filterType = [];
 		foreach($_GET['type'] ?? [] as $typeSlug){
 			$filterType []= TYPE::fromSlug($typeSlug);
 		}
+		$filterType = array_filter($filterType);
+
 		// license filter
 		$filterLicense = [];
 		foreach($_GET['license'] ?? [] as $licenseSlug){
 			$filterLicense []= LICENSE::fromSlug($licenseSlug);
 		}
+		$filterLicense = array_filter($filterLicense);
 
 		// quirk filter
 		$filterAvoidQuirk = [];
 		foreach($_GET['avoid'] ?? [] as $quirkSlug){
 			$filterAvoidQuirk []= QUIRK::fromSlug($quirkSlug);
 		}
+		$filterAvoidQuirk = array_filter($filterAvoidQuirk);
 
 		return new AssetQuery(
 			offset: intval($_GET['offset'] ?? 0),
 			limit: min(intval($_GET['limit'] ?? 150),500),
-			sort: SORTING::fromString($_GET['sort'] ?? "latest"),
+			sort: SORTING::fromAnyString($_GET['sort'] ?? "latest"),
 			filterAssetId: $filterAssetId,
 			filterTag: array_map('trim',array_filter(preg_split('/\s|,/',$_GET['q'] ?? ""))),
 			filterCreator: $filterCreator,
@@ -324,17 +343,25 @@ class AssetLogic{
 			$sqlValues = array_merge($sqlValues,$query->filterCreator);
 		}
 
-		if(isset($query->filterStatus)){
+		if($query->filterStatus !== NULL){
 			$sqlCommand .= " AND assetActive=? ";
 			$sqlValues []= $query->filterStatus;
 		}
 
 		// Sort
 		$sqlCommand .= match ($query->sort) {
+
+			// Options for public display
 			SORTING::LATEST => " ORDER BY assetDate DESC, assetId DESC ",
 			SORTING::OLDEST => " ORDER BY assetDate ASC, assetId DESC ",
 			SORTING::RANDOM => " ORDER BY RAND() ",
-			SORTING::POPULAR => " ORDER BY ( (assetClicks + 10) / POW( ABS( DATEDIFF( NOW(),assetDate ) ) + 1 , 1.3 ) ) DESC, assetDate DESC, assetId DESC "
+			SORTING::POPULAR => " ORDER BY ( (assetClicks + 10) / POW( ABS( DATEDIFF( NOW(),assetDate ) ) + 1 , 1.3 ) ) DESC, assetDate DESC, assetId DESC ",
+
+			// Options for internal editor (potentially less optimized)
+			SORTING::LEAST_CLICKED => " ORDER BY assetClicks ASC ",
+			SORTING::MOST_CLICKED => " ORDER BY assetClicks DESC ",
+			SORTING::LEAST_TAGGED => " ORDER BY (SELECT COUNT(*) FROM Tag WHERE Tag.assetId = Asset.assetId) ASC ",
+			SORTING::MOST_TAGGED => " ORDER BY (SELECT COUNT(*) FROM Tag WHERE Tag.assetId = Asset.assetId) DESC ",
 		};
 
 		// Offset and Limit
