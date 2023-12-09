ffee23dd3d36ab32bfa508975deeaee5f37dd074
Lennart Demes <31403260+struffel@users.noreply.github.com>
2023-12-09 13:10:44 +0100

    reformat editor-list without table

--- src/public-import/render/editor-list.php
+++ src/public-import/render/editor-list.php
@@ -5,64 +5,67 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/../include/init.php';
 $query = AssetQuery::fromHttpGet(filterStatus:NULL);
 $query->includeQuirks = true;
 $query->includeTags = true;
-$assets = AssetLogic::getAssets($query);
+$assets = AssetIoLogic::getAssets($query);
 ?>
-
-<table>
 
 <?php foreach ($assets->assets as $a) { ?>
 
-<tr id="<?=$a->id?>">
-	<form>
-		<td>
-			<a href="https://<?=implode('.', array_slice(explode('.', $_SERVER['HTTP_HOST']), -2));?>/go?id=<?=$a->id?>">
-				<img class="asset-image" alt="<?=$a->name?>" src="https://3d1-media.struffelproductions.com/file/3D-Assets-One/thumbnail/128-JPG-FFFFFF/<?=$a->id?>.jpg">
-			</a>
-		</td>
-		<td>
-			<input disabled type="text" name="id" value="<?=$a->id?>">
-			<input size="48" type="text" name="name" value="<?=$a->name?>">
-			<input size="48" type="text" name="tags" value="<?=implode(" ",$a->tags)?>">
-		</td>
-		<td>
-			<input size="32" type="text" name="url" value="<?=$a->url?>">
-			<input size="32" type="text" name="thumbnailUrl" value="<?=$a->thumbnailUrl?>">
-			<input type="date" name="date" value="<?=$a->date?>">
-		</td>
+<form 	hx-post="/render/editor-update.php" 
+		hx-target="#update-output-<?=$a->id?>"
+		hx-swap="innerHTML"
+		style="display: grid;grid-template-columns: 128px 256px 256px 256px 256px 128px 256px"
+>
+	<!--Hidden inputs-->
+	<input readonly type="hidden" name="creator" value="<?=$a->creator->value?>">
+	<input readonly type="hidden" name="id" value="<?=$a->id?>">
 
-		<td>
-			<select  name="status">
-				<?php foreach(ASSET_STATUS::cases() as $c){ ?>
-					<option <?= $a->status == $c ? 'selected' : '' ?> class="form-option" value="<?=$c->value?>"><?=$c->name?></option>
-				<?php } ?>
-			</select>
+	<!--Visible Inputs-->
+	<div>
+		<a href="https://<?=implode('.', array_slice(explode('.', $_SERVER['HTTP_HOST']), -2));?>/go?id=<?=$a->id?>">
+			<img class="asset-image" alt="<?=$a->name?>" src="https://3d1-media.struffelproductions.com/file/3D-Assets-One/thumbnail/128-JPG-FFFFFF/<?=$a->id?>.jpg">
+		</a>
+	</div>
+	<div>
+		<input size="48" type="text" name="name" value="<?=$a->name?>">
+		<input size="48" type="text" name="tagString" value="<?=implode(" ",$a->tags)?>">
+	</div>
+	<div>
+		<input size="32" type="text" name="url" value="<?=$a->url?>">
+		<input size="32" type="text" name="thumbnailUrl" value="<?=$a->thumbnailUrl?>">
+		<input type="date" name="date" value="<?=$a->date?>">
+	</div>
 
-			<select  name="type">
-				<?php foreach(TYPE::cases() as $c){ ?>
-					<option <?= $a->type == $c ? 'selected' : '' ?> class="form-option" value="<?=$c->value?>"><?=$c->name?></option>
-				<?php } ?>
-			</select>
-		
-			<select  name="license">
-				<?php foreach(LICENSE::cases() as $c){ ?>
-					<option <?= $a->license == $c ? 'selected' : '' ?> class="form-option" value="<?=$c->value?>"><?=$c->name?></option>
-				<?php } ?>
-			</select>
-		</td>
-		<td>
-			<select name="quirks" multiple>
-				<?php foreach(QUIRK::cases() as $c){ ?>
-					<option <?= in_array($c,$a->quirks) ? 'selected' : '' ?> class="form-option" value="<?=$c->value?>"><?=$c->name?></option>
-				<?php } ?>
-			</select>
-		</td>
-		<td>
-			<button>Update</button>
-			<pre><div>&nbsp;</div></pre>
-		</td>
-	</form>
-</tr>
+	<div>
+		<select  name="status">
+			<?php foreach(ASSET_STATUS::cases() as $c){ ?>
+				<option <?= $a->status == $c ? 'selected' : '' ?> class="form-option" value="<?=$c->value?>"><?=$c->name?></option>
+			<?php } ?>
+		</select>
+
+		<select  name="type">
+			<?php foreach(TYPE::cases() as $c){ ?>
+				<option <?= $a->type == $c ? 'selected' : '' ?> class="form-option" value="<?=$c->value?>"><?=$c->name?></option>
+			<?php } ?>
+		</select>
+	
+		<select  name="license">
+			<?php foreach(LICENSE::cases() as $c){ ?>
+				<option <?= $a->license == $c ? 'selected' : '' ?> class="form-option" value="<?=$c->value?>"><?=$c->name?></option>
+			<?php } ?>
+		</select>
+	</div>
+	<div>
+		<select name="quirks[]" multiple>
+			<?php foreach(QUIRK::cases() as $c){ ?>
+				<option <?= in_array($c,$a->quirks) ? 'selected' : '' ?> class="form-option" value="<?=$c->value?>"><?=$c->name?></option>
+			<?php } ?>
+		</select>
+	</div>
+	<div>
+		<button type="submit">Update</button>
+	</div>
+	<div id="update-output-<?=$a->id?>"></div>
+</form>
 
 <?php } ?>
 
-</table>
