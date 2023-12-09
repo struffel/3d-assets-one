24f33a2c5e31d6a927a6f57bf560491ef755da86
Lennart Demes <31403260+struffel@users.noreply.github.com>
2023-12-09 18:32:57 +0100

    Add reload button to editor

--- src/public-import/editor.php
+++ src/public-import/editor.php
@@ -9,7 +9,7 @@
 	<link rel="stylesheet" href=https://unpkg.com/chota />
 	<form	hx-get="/render/editor-list.php" 
 			hx-target="main" 
-			hx-trigger="change,load,input delay:200ms" 
+			hx-trigger="submit,change,load,input delay:200ms" 
 			hx-swap="innerHTML"
 			style="display: flex;"
 	>
@@ -32,6 +32,7 @@
 			<?php } ?>
 		</select>
 		<input type="number" name="offset" placeholder="offset">
+		<button type="submit">Reload</button>
 	</form>
 	<hr>
 	<main>
