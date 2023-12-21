f832db2f94ccbbc457c351f8c04049df096da189
Lennart Demes <31403260+struffel@users.noreply.github.com>
2023-12-21 16:12:46 +0100

    Rework the cronjob to run via one file

---
+++ src/public-import/cron.php
@@ -0,0 +1,80 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/../include/init.php';
+
+LogLogic::initialize("Performing cronjob");
+
+$allActions = !isset($_GET['action']);
+$action = $_GET['action'] ?? NULL;
+
+try{
+	
+	// Activate one or multiple assets
+
+	if($allActions | $action = "activate" ){
+
+		$maxNumberOfAssets = max(1,intval($_GET['number']));
+
+		$query = new AssetQuery(
+			filterStatus:  ASSET_STATUS::INACTIVE,
+			limit: $maxNumberOfAssets,
+			sort: SORTING::RANDOM,
+		);
+	
+		$assetsToActivate = AssetLogic::getAssets($query);
+		foreach ($assetsToActivate->assets as $a) {
+	
+			DatabaseLogic::startTransaction();
+
+			LogLogic::write("Getting thumbnail for asset ". $a->id." from url ".$a->thumbnailUrl);
+	
+			$creatorFetcher = CreatorFetcher::fromCreator($a->creator);
+			$imageData = $creatorFetcher->fetchThumbnailImage($a->thumbnailUrl);
+	
+			ImageLogic::buildAndUploadThumbnailsToBackblazeB2($a,$imageData);
+			$a->status = ASSET_STATUS::ACTIVE;
+			AssetLogic::saveAssetToDatabase($a);
+	
+			DatabaseLogic::commitTransaction();
+		}
+	}
+
+	// Refresh one creator
+
+	if($allActions | $action = "refresh"){
+
+		if(isset($_GET['creatorId'])){
+			$creatorId = intval($_GET['creatorId']);
+		}else{
+			$randomTargets = explode(",",getenv("3D1_REFRESH_CREATOR_ID"));
+			$randomIndex = array_rand($randomTargets);
+			$creatorId = $randomTargets[$randomIndex];
+		}
+
+		$maxNumberOfAssets = intval($_GET['max']??1);
+		LogLogic::write("Refreshing Creator: $creatorId");
+
+		$creator = CreatorFetcher::fromCreator(CREATOR::from($creatorId));
+		LogLogic::write("Created creator object.");
+		$result = $creator->runUpdate();
+
+		LogLogic::write("Found ".sizeof($result->assets)." new assets");
+		if(sizeof($result->assets) > 0){
+			LogLogic::write("Writing new assets to DB:");
+			foreach ($result->assets as $a) {
+				DatabaseLogic::startTransaction();
+				$a->status = ASSET_STATUS::INACTIVE;	// Failsave in case the creator fetching function does not set it properly.
+				AssetLogic::saveAssetToDatabase($a);
+				DatabaseLogic::commitTransaction();
+			}
+			LogLogic::write("Wrote ".sizeof($result->assets)." new assets.");
+		}
+	}
+
+	if( $allActions | $action = "livecheck"){
+		// Coming soon
+	}
+	
+}finally{
+	LogLogic::echoCurrentLog();
+}
