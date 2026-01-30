1866fa6c3895e23ee41feedeab1e4eea4c90c50d
Lennart Demes <31403260+struffel@users.noreply.github.com>
2026-01-30 18:18:11 +0100

    Update phpinfo.php

--- src/public/admin/phpinfo.php
+++ src/public/admin/phpinfo.php
@@ -1,8 +1,26 @@
 <?php
 
 use misc\Auth;
+use blocks\AdminHeaderBlock;
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/../include/init.php';
 
 Auth::requireAuth();
-phpinfo();
+AdminHeaderBlock::render();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="/css/base.css">
+	<title>PHP info</title>
+</head>
+
+<body>
+	<?php phpinfo(); ?>
+</body>
+
+</html>
