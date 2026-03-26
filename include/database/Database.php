4136c165bf49a498849add7530ef35d65f562ec1
Lennart Demes <31403260+struffel@users.noreply.github.com>
2026-03-26 15:46:45 +0100

    Update Database.php

--- include/database/Database.php
+++ include/database/Database.php
@@ -193,7 +193,7 @@ class Database
 		LEFT JOIN (SELECT creatorId, COUNT(*) as creatorCountLast90d FROM Asset WHERE date >= datetime('now', '-90 days') GROUP BY creatorId) as recentAssets ON Asset.creatorId = recentAssets.creatorId;";
 		$result = Database::runQuery($sql);
 
-		if ($result === false) {
+		if (is_bool($result)) {
 			throw new Exception("Failed to retrieve assets for popularity score update.");
 		}
 
