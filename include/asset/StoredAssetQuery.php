021758dbe96d722652585fcd81fff01b033cbfa8
Lennart Demes <31403260+struffel@users.noreply.github.com>
2026-03-26 15:58:26 +0100

    Update StoredAssetQuery.php

--- include/asset/StoredAssetQuery.php
+++ include/asset/StoredAssetQuery.php
@@ -45,10 +45,15 @@ class StoredAssetQuery
 	}
 
 	/** @return array<int, int> */
-	public static function assetCountByCreator(): array
+	public static function assetCountByCreator(?int $lastNDays = null): array
 	{
 		$assetCountByCreator = [];
-		$result = Database::runQuery("SELECT creatorId, COUNT(*) AS count FROM Asset GROUP BY creatorId");
+		$sql = "SELECT creatorId, COUNT(*) AS count FROM Asset";
+		if ($lastNDays !== null) {
+			$sql .= " WHERE date >= datetime('now', '-$lastNDays days')";
+		}
+		$sql .= " GROUP BY creatorId";
+		$result = Database::runQuery($sql);
 		if (is_bool($result)) {
 			return $assetCountByCreator;
 		}
